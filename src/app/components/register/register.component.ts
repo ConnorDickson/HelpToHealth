3b4b7a2036a32f4a6838125aeb4735b0dabda3f0
Connor Dickson <cdickson14@qub.ac.uk>
2020-04-23 20:16:42 +0100

    Register as

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,9 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RegisterComponent implements OnInit {
-
-  Roles: any = ['Admin', 'Author', 'Reader'];
-
+  selected = 'Volunteer';
   constructor() { }
 
   ngOnInit() {
